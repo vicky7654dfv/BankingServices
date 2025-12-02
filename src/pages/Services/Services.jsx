873a40e41a7a4e849532d4001e7c320eb09613a9
vicky7654dfv <vignesht2013@gmail.com>
2025-12-02 16:00:15 +0530

    change 1

--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import Header from '../../components/Header/Header'
-import Footer from '../../components/Footer/Footer'
-import ServicesHero from '../../components/ServicesHero/Serviceshero'
-import ServiceCategories from '../../components/ServiceCategories/ServiceCategories'
-import AccountTypes from '../../components/AccountTypes/AccountTypes'
-import InsuranceSchemes from '../../components/InsuranceSchemes/InsuranceSchemes'
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import ServiceCategories from "../../components/ServiceCategories/ServiceCategories";
+import AccountTypes from "../../components/AccountTypes/AccountTypes";
+import InsuranceSchemes from "../../components/InsuranceSchemes/InsuranceSchemes";
+import ServicesHero from "../../components/ServicesHero/ServicesHero";
 
 export default function Services() {
   return (
     <div>
-        <Header />
-        <ServicesHero />
-        <ServiceCategories />
-        <AccountTypes />
-        <InsuranceSchemes />
-        <Footer />
+      <Header />
+      <ServicesHero />
+      <ServiceCategories />
+      <AccountTypes />
+      <InsuranceSchemes />
+      <Footer />
     </div>
-  )
+  );
 }
